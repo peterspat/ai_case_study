0450bebc7d0585025814d3eac1d3afbf10af6b40
peterspat <patrick.petersen91@googlemail.com>
2024-05-03 22:40:09 +0200

    up

--- src/taipy_example.py
+++ src/taipy_example.py
@@ -18,7 +18,7 @@ penguin_species_selector = """
 
 if __name__ == "__main__":
         Gui(page=penguin_species_selector).run( host="127.0.0.1",
-                port=1234,
+                port=5000,
                 dark_mode=True,
                 debug=True,
                 use_reloader=True)
