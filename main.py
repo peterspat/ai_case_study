617ce7ae28c9a161a7ffeb9881e284e0062f8711
Patrick Petersen <petersen@fzi.de>
2024-05-07 09:51:06 +0200

    update

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import json
 import os
 import pickle
 from collections import Counter
+from copy import copy
 from multiprocessing import cpu_count, Pool
 from gensim import corpora, models
 import matplotlib.pyplot as plt
 import scienceplots
+from sklearn import metrics
+from sklearn.cluster import KMeans
 
 plt.style.use('science')
 
@@ -748,6 +751,142 @@ def preprocess(df):
     df['blog_post'] = df['blog_post'].apply(lambda x: [word for word in x if word not in stop_words_en])
 
 
+    ###
+
+    # Remove modalpartikeln
+    modalpartikeln = [
+        "ja", "doch", "eben", "halt", "denn", "wohl", "mal", "schon", "bloß", "auch",
+        "nur", "eigentlich", "etwa", "immerhin", "ebenso", "wirklich", "immer", "aber",
+        "gerade", "ruhig", "jedoch", "genau", "dann", "zumindest", "wenigstens",
+        "wahrscheinlich", "manchmal", "vielleicht", "schließlich", "tatsächlich",
+        "ungefähr", "gar", "sowieso", "keineswegs", "möglicherweise", "hoffentlich",
+        "außerdem", "irgendwie", "sogar", "fast", "lediglich", "mal", "meistens",
+        "nun", "scheinbar", "bisweilen", "freilich", "selten", "durchaus", "generell",
+        "letztlich", "endlich", "offenbar", "jedenfalls", "zunächst", "beinahe", "also",
+        "geradezu", "allenfalls", "gewiss", "definitiv", "kaum", "bislang", "notfalls",
+        "umso", "allemal", "schier", "bestenfalls", "vorerst", "einigermaßen",
+        "glücklicherweise", "nachweislich", "vor allem", "inzwischen", "unter Umständen"
+    ]
+    modal_particle_word_de = set(modalpartikeln)
+
+    dict_file = "modal_particle_word_de.pkl"
+    if os.path.exists(dict_file):
+        # Load the dictionary from file
+        with open(dict_file, "rb") as f:
+            sorted_dic = pickle.load(f)
+    else:
+        # If the dictionary file doesn't exist, create it
+        new = df['blog_post'].values.tolist()
+        corpus = [word for i in new for word in i]
+
+        # Assuming stop_words_de is defined somewhere
+        dic = defaultdict(int)
+        for word in corpus:
+            if word in modal_particle_word_de:
+                dic[word] += 1
+
+        # Sort the dictionary by value (frequency) in descending order
+        sorted_dic = dict(sorted(dic.items(), key=lambda item: item[1], reverse=True))
+
+        # Save the dictionary to file
+        with open(dict_file, "wb") as f:
+            pickle.dump(sorted_dic, f)
+
+    # Select the top x stopwords
+    top_x = 10  # You can change this value to plot more or fewer stopwords
+    top_stopwords = dict(list(sorted_dic.items())[:top_x])
+
+    # Extract stopwords and their frequencies
+    stopwords_list = list(top_stopwords.keys())[::-1]
+    frequencies = list(top_stopwords.values())[::-1]
+
+    # Create a bar plot
+    fig = go.Figure(data=[go.Bar(x=frequencies, y=stopwords_list, orientation='h')])
+
+    # Customize layout
+    fig.update_layout(
+        title='Top {} Modal Particle'.format(top_x),
+        xaxis=dict(title='Modal Particle'),
+        yaxis=dict(title='Frequency')
+    )
+
+    # Show the plot
+    fig.show()
+    # Create horizontal bar plot using Matplotlib
+    plt.subplots(figsize=(6, 4), layout='constrained')
+    plt.barh(stopwords_list, frequencies)
+    plt.title('Top {} Modal Particle'.format(top_x))
+    plt.xlabel('Frequency')
+    plt.ylabel('Modal Particle')
+    #plt.gca().invert_yaxis()  # Invert y-axis to have highest frequency at the top
+
+    plt.savefig(f'modal_particle_word_de.png', dpi=300)
+    plt.show()
+
+
+
+
+    ######## plot remaining words:
+    counter_file = "remaining_words_after_modal_particle_word_de.pkl"
+    if os.path.exists(counter_file):
+        # Load the Counter from file
+        with open(counter_file, "rb") as f:
+            counter = pickle.load(f)
+    else:
+        # If the Counter file doesn't exist, create it
+        new = df['blog_post'].values.tolist()
+
+        corpus = [word for sublist in new for word in sublist if isinstance(word, str)]
+
+        # Assuming stop_words_de is defined somewhere
+        dic = defaultdict(int)
+        for word in corpus:
+            if word in modal_particle_word_de:
+                dic[word] += 1
+
+        counter = Counter(corpus)
+
+        # Save the Counter to file
+        with open(counter_file, "wb") as f:
+            pickle.dump(counter, f)
+    most = counter.most_common()
+
+    top_n_words = 10
+    x, y = [], []
+    for word, count in most:
+        if (word not in modal_particle_word_de):
+            x.append(word)
+            y.append(count)
+
+
+    # Create a bar plot
+    fig = go.Figure(data=[go.Bar(x=y[:top_n_words][::-1], y=x[:top_n_words][::-1], orientation='h')])
+
+    # Customize layout
+    fig.update_layout(
+        title='Top {} Non-Modal-Particle'.format(top_x),
+        xaxis=dict(title='Non-Modal-Particle'),
+        yaxis=dict(title='Frequency')
+    )
+
+    # Show the plot
+    fig.show()
+    plt.subplots(figsize=(6, 4), layout='constrained')
+    plt.barh(x[:top_n_words][::-1], y[:top_n_words][::-1])
+    plt.title('Top {} Non-Stop'.format(top_n_words))
+    plt.xlabel('Non-Modal-Particle')
+    plt.ylabel('Frequency')
+    #plt.gca().invert_yaxis()  # Invert y-axis to have highest frequency at the top
+    plt.savefig(f'remaining_words_after_modal_particle_word_de.png', dpi=300)
+    plt.show()
+
+
+    df['blog_post'] = df['blog_post'].apply(lambda x: [word for word in x if word not in modal_particle_word_de])
+
+
+
+
+
 
 
 
@@ -787,6 +926,226 @@ def preprocess(df):
     return df
 
 
+def evaluation(df):
+    word_counts = df['blog_post'].apply(len)
+    hist_data, bins = np.histogram(word_counts, bins='auto')
+    fig = go.Figure(data=[go.Bar(x=bins, y=hist_data)])
+    fig.update_layout(title='Word Count Histogram of Each Post',
+                      xaxis_title='Number of Words',
+                      yaxis_title='Frequency')
+    fig.show()
+
+    # Assuming you have already defined bins and hist_data
+    plt.subplots(figsize=(6, 4), layout='constrained')
+    # plt.hist(hist_data, bins=bins)
+    plt.hist(bins[:-1], bins, weights=hist_data)
+
+    plt.title('Word Count Histogram of Each Post')
+    plt.xlabel('Number of Words')
+    plt.ylabel('Frequency')
+    # fig.subplots_adjust(left=0.15, top=0.95)
+    plt.savefig(f'evaluation_word_count_histogram.png', dpi=300)
+    plt.show()
+
+
+
+    word_counts = df['blog_post'].apply(len)
+
+    # Create a DataFrame for Plotly
+    data = pd.DataFrame({'Word Count': word_counts})
+
+    # Create a violin plot using Plotly
+    fig = px.violin(data, y='Word Count', box=True, points='all')
+    fig.update_layout(title='Word Count of Each Post Violin Plot',
+                      yaxis_title='Number of Words')
+    fig.show()
+
+    # Count the number of words in each row of the specified column
+    word_counts = df["blog_post"].apply(len)
+
+    # Create a DataFrame for Matplotlib
+    data = pd.DataFrame({'Word Count': word_counts})
+
+    # Create violin plot using Matplotlib
+    plt.subplots(figsize=(4, 6), layout='constrained')
+    plt.violinplot(data['Word Count'], showmeans=True, showextrema=True)
+    plt.title('Word Count of Each Post Violin Plot')
+    plt.xlabel('Posts')
+    plt.ylabel('Number of Words')
+
+    plt.savefig(f'evaluation_word_count_violin_plot.png', dpi=300)
+    plt.show()
+
+
+    n = 3
+    filename = f"evaluation_{n}_ngram_result.json"
+    if os.path.exists(filename):
+        with open(filename, 'r') as file:
+            top_nrams = json.load(file)
+    else:
+        comb = df['blog_post'].apply(lambda x: ' '.join(x))
+        top_nrams = get_top_ngram(comb, n=n)
+        top_nrams = [(item[0], int(item[1])) for item in top_nrams]
+
+        with open(filename, 'w') as file:
+            json.dump(top_nrams, file)
+    top_n = 30
+    top_nrams = top_nrams[:top_n]
+    x, y = map(list, zip(*top_nrams))
+    # Reverse the order of the data
+    x = x[::-1]
+    y = y[::-1]
+    # Create a bar plot using Plotly
+    fig = go.Figure(data=[go.Bar(x=y, y=x, orientation='h')])
+    fig.update_layout(title='Bar Plot', xaxis_title='Count', yaxis_title='Category')
+    fig.show()
+
+    # Create horizontal bar plot using Matplotlib
+    plt.subplots(figsize=(6, 4), layout='constrained')
+    plt.barh(x, y)
+    plt.title('Bar Plot')
+    plt.xlabel('Count')
+    plt.ylabel('Category')
+    plt.savefig(f'evaluation_n_gram.png', dpi=300)
+    plt.show()
+
+    ##################################
+
+    filename = f"evaluation_polarity_score.csv"
+    if os.path.exists(filename):
+        df_pol = pd.read_csv(filename)
+    else:
+        # df['polarity_score'] = df['blog_post'].apply(lambda x: polarity(x))
+
+        # Applying polarity calculation row-wise in parallel
+        df_pol = copy(df)
+        df_pol['blog_post'] = df['blog_post'].apply(lambda x: ' '.join(x))
+        df_pol['polarity_score'] = parallelize_dataframe(df_pol, compute_polarity)
+        df_pol.to_csv(filename, index=False)
+
+    # Calculate polarity_score using polarity function
+
+    # Define the number of bins
+    num_bins = 10
+
+    # Calculate bin size dynamically
+    bin_size = 2 / num_bins
+
+    # Create a histogram trace
+    # Create a histogram trace
+    trace_hist = go.Histogram(x=df_pol['polarity_score'],
+                              xbins=dict(start=-1, end=1, size=bin_size),
+                              hoverinfo='y+text',  # Show count and custom text on hover
+                              # hovertemplate='Bin Range: %{x:.2f} -%{x+x:.2f} <br>Count: %{y}')
+                              hovertemplate='Count: %{y}')
+
+    # Create layout for histogram
+    layout_hist = go.Layout(title='Polarity Score Distribution',
+                            xaxis=dict(title='Polarity Score'),
+                            yaxis=dict(title='Count'))
+
+    # Create histogram figure
+    fig_hist = go.Figure(data=[trace_hist], layout=layout_hist)
+
+    # Show the histogram
+    fig_hist.show()
+
+    plt.subplots(figsize=(6, 4), layout='constrained')
+    plt.hist(df_pol['polarity_score'], bins=int((1 - (-1)) / bin_size), range=(-1, 1), edgecolor='black')
+    plt.title('Polarity Score Distribution')
+    plt.xlabel('Polarity Score')
+    plt.ylabel('Count')
+    plt.grid(True)
+    plt.savefig(f'evaluation_polarity_score.png', dpi=300)
+    plt.show()
+
+    # Calculate polarity using sentiment function
+    df_pol['polarity'] = df_pol['polarity_score'].map(lambda x: sentiment(x))
+    # Calculate the percentage of each polarity
+    polarity_percentages = df_pol['polarity'].value_counts()  # (normalize=True) * 100
+
+    # Create a bar plot
+    trace_bar = go.Bar(x=polarity_percentages.index,
+                       y=polarity_percentages.values)
+
+    # Create layout for bar plot
+    layout_bar = go.Layout(title='Polarity Distribution',
+                           xaxis=dict(title='Polarity'),
+                           yaxis=dict(title='Count'))
+
+    # Create bar plot figure
+    fig_bar = go.Figure(data=[trace_bar], layout=layout_bar)
+
+    # Show the bar plot
+    fig_bar.show()
+
+    plt.subplots(figsize=(6, 4), layout='constrained')
+    plt.bar(polarity_percentages.index, polarity_percentages.values)
+    plt.title('Polarity Distribution')
+    plt.xlabel('Polarity')
+    plt.ylabel('Count')
+
+    plt.grid(True)
+    plt.savefig(f'evaluation_polarity.png', dpi=300)
+    plt.show()
+
+
+
+
+
+
+
+    count_vect = CountVectorizer()
+    comb = df['only_adj_noun_propn'].apply(lambda x: ' '.join(x))
+    bow = count_vect.fit_transform(comb.values)
+    terms = count_vect.get_feature_names_out()
+    print(terms[1:10])
+    n_cluster = 10
+    model = KMeans(n_clusters=n_cluster, init='k-means++', random_state=42)
+    model.fit(bow)
+    labels = model.labels_
+    cluster_center = model.cluster_centers_
+    silhouette_score = metrics.silhouette_score(bow, labels, metric='euclidean')
+    df['labels'] = model.labels_  # the last column you can see the label numebers
+    df.head(2)
+    print(df.groupby(['labels']).count())
+    print("Top terms per cluster:")
+    order_centroids = model.cluster_centers_.argsort()[:, ::-1]
+    terms = count_vect.get_feature_names_out()
+    for i in range(n_cluster):
+        print("Cluster %d:" % i, end='')
+        for ind in order_centroids[i, :n_cluster]:
+            print(' %s' % terms[ind], end='')
+            print()
+    plt.subplots(figsize=(6, 4), layout='constrained')
+    plt.bar([x for x in range(n_cluster)], df.groupby(['labels'])['only_adj_noun_propn'].count(), alpha=0.4)
+    plt.title('KMeans cluster points')
+    plt.xlabel("Cluster number")
+    plt.ylabel("Number of points")
+    plt.savefig(f'CountVectorizer_kmeans_only_adj_noun_propn.png', dpi=300)
+    plt.show()
+
+
+    dic = corpora.Dictionary(df['only_adj_noun_propn'])
+    bow_corpus = [dic.doc2bow(doc) for doc in df['only_adj_noun_propn']]
+
+    lda_model = models.LdaMulticore(bow_corpus,
+                                    random_state=42,
+                                           num_topics=10,
+                                           id2word=dic,
+                                            chunksize=20,
+                                           passes=10,
+                                    iterations=40,
+                                           workers=5)
+
+
+    print(lda_model.show_topics())
+
+    #pyLDAvis.enable_notebook()
+    #vis = pyLDAvis.gensim.prepare(lda_model, bow_corpus, dic)
+    x=1
+
+
 def main():
 
     download_missing_nltk_dataset()
@@ -817,9 +1176,9 @@ def main():
         df = preprocess(df)
         df.to_csv(filename, index=False)
 
-    #df=df[:10]
     comb = df['blog_post'].apply(lambda x: ' '.join(x))
 
+    #df=df[:10]
 
     #tagger_de = ht.HanoverTagger('morphmodel_ger.pgz')
     #words = nltk.word_tokenize("Jahr ja wir es und mal schon")
@@ -852,25 +1211,8 @@ def main():
         df.to_csv(filename, index=False)
 
 
+    evaluation(df)
 
-    dic = corpora.Dictionary(df['only_adj_noun_propn'])
-    bow_corpus = [dic.doc2bow(doc) for doc in df['only_adj_noun_propn']]
-
-    lda_model = models.LdaMulticore(bow_corpus,
-                                    random_state=42,
-                                           num_topics=10,
-                                           id2word=dic,
-                                            chunksize=20,
-                                           passes=10,
-                                    iterations=40,
-                                           workers=5)
-
-
-    print(lda_model.show_topics())
-
-    #pyLDAvis.enable_notebook()
-    #vis = pyLDAvis.gensim.prepare(lda_model, bow_corpus, dic)
-    x=1
 
 
 
